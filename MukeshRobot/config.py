a64e0ed2bd1d0aaa1e0721f4fe3d7de7748d4a7b
𝐈𝐍𝐍𝐎𝐂𝐄𝐍𝐓 𝐁𝐎𝐘 <155868717+INNOCENTBOY2926@users.noreply.github.com>
2024-12-26 22:56:14 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -4,21 +4,21 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "17763538" # integer value, dont use ""
-    API_HASH = "babad31b4b434fc53d8bd13370c556c3"
-    TOKEN = "6894743901:AAFlsaTrSwxaFpYMVXeXpkbr3NR_Nimoepw"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6961211249 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    API_ID = "" # integer value, dont use ""
+    API_HASH = ""
+    TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID =  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "THE_FUCKER_BOTS_2926"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = ""  # Your own group for support, do not add the @
     START_IMG = "https://files.catbox.moe/jxk5hn.jpg"
-    EVENT_LOGS = (-1002108743436)  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
-    MONGO_DB_URI= "mongodb+srv://nakuldkdhacker:nakuldkdhacker$100@cluster0.45znzoj.mongodb.net/"
+    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    MONGO_DB_URI= ""
     # RECOMMENDED
     DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
-        "9VTTT9B4QHV8DIDS"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "A8RZUCKYOVRS"
+    TIME_API_KEY = ""
     # Get your API key from https://timezonedb.com/api
 
     # Optional fields
